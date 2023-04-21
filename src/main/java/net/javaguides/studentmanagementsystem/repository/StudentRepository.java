7d9cdb2e0d9c49db0fbde082b433329328e984ae
Héctor Castillo <100151419+Wenzeltelger@users.noreply.github.com>
2023-04-21 16:19:52 -0600

    uploading the file

---
+++ src/main/java/net/javaguides/studentmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package net.javaguides.studentmanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.studentmanagementsystem.entity.Student;
+
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    /* */
+    
+}
+
